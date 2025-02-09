08018852d9bd53d27327da65f4c28bf8596dbef0
abuzar0809 <abdulazizxontoshpulatov@gmail.com>
2025-02-09 19:11:03 +0500

    succes

---
+++ main.js
@@ -0,0 +1,27 @@
+fetch('https://6560acc483aba11d99d151a5.mockapi.io/api/movies')
+.then(response => response.json())
+.then(data => {
+    data.forEach(movie =>{
+        console.log(movie)
+        let box = document.querySelector('.container')
+        box.innerHTML +=
+        ` <div class="row">
+        <div class="col ">
+          <div class="card">
+            <div class="card-image">
+              <img src=${movie.movieImg}>
+              <h1 class="card-title">${movie.movieName}</h1>
+            </div>
+            <div class="card-content">
+              <p>${movie.starring}</p>
+            </div>
+            <div class="card-action">
+              <a href=${movie.trailer} target="_blank">Whatch trailer</a>
+            </div>
+          </div>
+        </div>
+      </div>`
+       
+        
+    })
+})
